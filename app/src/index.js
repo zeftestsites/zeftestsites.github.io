fbe2e8f4f186644ebec13b98195c6fb5014341ca
dkfann <danny.k.fann@gmail.com>
2019-09-21 00:23:59 -0700

    Initial working scroll site

---
+++ app/src/index.js
@@ -0,0 +1,60 @@
+// This is the main js file that will import everything to be bundled.
+import './js/index.js';
+import './less/styles.less';
+import 'intersection-observer';
+import scrollama from 'scrollama';
+
+const scroller = scrollama();
+
+console.log(scroller);
+const container = document.getElementById('description-container');
+
+// scroller
+//     .setup({
+//         step: '.main-content'
+//     })
+//     .onStepEnter(({element, index, direction}) => {
+//         console.log(element)
+//     })
+//     .onStepExit(({element, index, direction}) => {
+//         console.log(index)
+//     });
+
+scroller
+    .setup({
+        step: '.step',
+    })
+    .onStepEnter(({element, index, direction}) => {
+        container.classList.remove('unfade');
+        container.classList.add('fade');
+        element.classList.add('focus');
+        element.classList.remove('unfocus');
+        // console.log(container.querySelectorAll('.description-image').forEach(image => console.log(image.classList)));
+
+        container.querySelectorAll('.description-image').forEach(image => {
+            image.classList.remove('shown');
+            image.classList.add('hidden');
+        });
+
+        console.log(element);
+
+        container.querySelector(`#${element.id}-image`).classList.add('shown');
+    })
+    .onStepExit(({element, index, direction}) => {
+        container.classList.remove('fade');
+        container.classList.add('unfade');
+        element.classList.remove('focus');
+        element.classList.add('unfocus');
+        // container.querySelector('.description-image').classList.remove('shown');
+        // container.querySelector('.description-image').classList.add('hidden');
+
+        container.querySelectorAll('.description-image').forEach(image => {
+            image.classList.remove('shown');
+        });
+
+        container.querySelector(`#${element.id}-image`).classList.remove('shown');
+
+        if (element.id === 'hci' && direction === 'up') {
+            container.querySelector('#main-image').classList.add('shown');
+        }
+    })
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: [
+        './app/src/index.js',
+    ],
+    output: {
+        path: path.resolve(__dirname, 'app/dist/'),
+        filename: '[name].js',
+    },
+
+    target: 'web',
+
+    // WEBPACK DEV SERVER
+    devServer: {
+        publicPath: '/',
+        contentBase: path.join(__dirname, 'app/src/'),
+        port: 8085,
+    },
+
+    // MODULE LOADERS
+    module: {
+        rules: [
+            // Transpile JS using babel loader
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            // Compile less to css
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'less-loader',
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/fonts/'
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use: {
+                    loader: 'file-loader?limit=8192',
+                },
+            },
+        ],
+    },
+
+    // PLUGINS
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+        new HtmlWebpackPlugin({
+            template: './app/src/index.html',
+            filename: 'index.html',
+        }),
+    ],
+};
